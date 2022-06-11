0e83a7e6e657074a866c8d9e7f1e5c2990f2406a
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-11 00:29:06 -0500

    added functionality and still need to work on ui

--- index.js
+++ index.js
@@ -1,66 +1,61 @@
-const gameElement = 
-["rock", "paper", "scissor"]
+const gameElement = ["rock", "paper", "scissor"]
 
-const computerPlay = () => {
-    return gameElement
-    [Math.floor(
-        Math.random()*
-        gameElement.length)]
-}
+const computerPlay = () => {return gameElement[Math.floor(Math.random()*gameElement.length)]}
 
 let output = ""
+let result = document.getElementById('result')
+let winCount = document.getElementById('winCount')
+let finalWinner = document.getElementById('finalWinner')
+
+let playerWinCount = 0
+let computerWinCount = 0
+let tie = 0
 
 const singleRound = () => {
-    const playerSelection = 
-    prompt("What's your input?")
+    
+    let playerSelection = []
+    const getBtn = document.querySelectorAll('button')
+        getBtn.forEach((item) => {item.addEventListener('click', function(e) {
+                                    playerSelection.push(e.target.textContent)})
+                                })
+    
     const computerSelection = computerPlay()
 
-if (playerSelection.toLowerCase() === "rock") 
-    {
-    output = computerSelection === "rock" ? 
-    output = "It's a tie" : 
-    computerSelection === "paper" ? 
-    output = "You lose, Computer won!" :
-    output = "You win!, Computer lose"
-    } else if 
-    (playerSelection.toLowerCase() === "paper") 
-    {
-    output = computerSelection === "paper" ? 
-    output = "It's a tie" : 
-    computerSelection === "scissor" ? 
-    output = "You lose, Computer won!" :
-    output = "You win!, Computer lose"
-    } else if
-    (playerSelection.toLowerCase() === "scissor") 
-    {
-    output = computerSelection === "scissor" ? 
-    output = "It's a tie" : 
-    computerSelection === "rock" ? 
-    output = "You lose, Computer won!" :
-    output = "You win!, Computer lose"
+    if (document.getElementsByTagName("button")[0].id === "rock") {
+        
+        output = computerSelection === "rock" ? output = "It's a tie" : 
+        computerSelection === "paper" ? output = "You lose, Computer won!" :
+        output = "You win!, Computer lose"
+
+    } else if (document.getElementsByTagName("button")[0].id === "paper") {
+        
+        output = computerSelection === "paper" ? output = "It's a tie" : 
+        computerSelection === "scissor" ? output = "You lose, Computer won!" :
+        output = "You win!, Computer lose"
+
+    } else if(document.getElementsByTagName("button")[0].id === "scissor") {
+        
+        output = computerSelection === "scissor" ? output = "It's a tie" : 
+        computerSelection === "rock" ? output = "You lose, Computer won!" :
+        output = "You win!, Computer lose"
+    
     }
-    alert (output)
+
+    if (output === "You win!, Computer lose") {playerWinCount += 1} 
+    else if (output === "It's a tie") {tie += 1}
+    else {computerWinCount += 1}
+    
+    if (playerWinCount < 5 && computerWinCount < 5) {
+        result.textContent = output
+        winCount.textContent = `Win: ${playerWinCount} Lose: ${computerWinCount} Tie: ${tie}`
+    
+    } else if (playerWinCount === 5) {
+        winCount.textContent = "You Win!!"
+    
+    } else if (computerWinCount === 5) {
+        winCount.textContent = "Computer Win!!"
+        
+    }
 }
 
-const game = () => {
-    let playerWinCount = 0
-    let computerWinCount = 0
-    let tie = 0
-
-    for (let i=0; i<5; i++) {
-        singleRound ()
-        
-        if (output === "You win!, Computer lose") {
-            playerWinCount += 1
-        } else if (output === "It's a tie") {
-            tie += 1
-        }
-        else (computerWinCount += 1)
-        
-        alert (`Win: ${playerWinCount} Lose: ${computerWinCount} Tie: ${tie}`)
-    }  
-}
-
-game ()
-
-
+window.addEventListener('click', singleRound)
